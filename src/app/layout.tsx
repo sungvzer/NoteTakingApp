bbc72f5d042010300d1fa8cd4af3a17e7a4118a3
sungvzer <sungvzer@proton.me>
2024-04-13 10:49:50 +0200

    feat: add simple landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="">
-      <body className={inter.className}>
+      <body className={`${inter.className} flex flex-col h-screen`}>
         <Navbar />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col items-center p-10 text-center flex-grow justify-center">
+      <h1 className="text-3xl font-bold md:text-4xl">
+        Welcome to Noted, <br /> an innovative note-taking app.
+      </h1>
+      <p className="text-lg mt-4 md:text-xl">
+        Noted is a note-taking app that is <em>innovative</em> and easy to use.
+      </p>
+      <button className="mt-8 bg-emerald-700 text-white rounded-lg px-4 py-2 hover:bg-emerald-800">
+        Get started
+      </button>
+    </main>
   );
 }
